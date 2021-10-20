333c498eec8a85a51c1cf26551cfcbb037f4ec8c
曾志威 <>
2021-10-20 23:40:16 +0800

    bio multi input

--- src/bio/Client.java
+++ src/bio/Client.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 9999);
+        System.out.println("127.0.0.1:9999 start...");
         OutputStream os = socket.getOutputStream();
 
         PrintStream ps = new PrintStream(os);
-
-        ps.print("hello");
-        //ps.flush();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String msg = scanner.nextLine();
+            ps.println(msg);
+            ps.flush();
+        }
     }
 }
--- src/bio/Server.java
+++ src/bio/Server.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args)  {
         try {
+            System.out.println("listening at port 9999...");
             ServerSocket serverSocket = new ServerSocket(9999);
             Socket socket = serverSocket.accept();
             // 从socket管道得到一个字节输入流对象

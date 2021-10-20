3a48e978ee45e03198f47509101e61b642cd41db
曾志威 <>
2021-10-20 23:33:04 +0800

    bio simple

---
+++ src/bio/Client.java
@@ -0,0 +1,18 @@
+package bio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 9999);
+        OutputStream os = socket.getOutputStream();
+
+        PrintStream ps = new PrintStream(os);
+
+        ps.print("hello");
+        //ps.flush();
+    }
+}
